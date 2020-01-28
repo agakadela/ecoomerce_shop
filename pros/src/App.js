24eec3cb2f6fd1a16d3b5043ee97e1cbef5e8c90
woziu17 <agn.woz@gmail.com>
2020-01-28 18:54:56 +0100

    observer pattern included

--- pros/src/App.js
+++ pros/src/App.js
@@ -24,20 +24,22 @@ class App extends Component {
   componentDidMount() {
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(
+      async userAuth => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
+          userRef.onSnapshot(snapShot => {
+            setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
+            });
           });
-        });
-      } else {
+        }
         setCurrentUser(userAuth);
-      }
-    });
+      },
+      error => console.log(error)
+    );
   }
 
   componentWillUnmount() {
